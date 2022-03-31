1fdae0a3ea2aad210a0d12e5c5ee9f12291f4f10
YuWang24 <u5762606@anu.edu.au>
2022-03-31 21:18:06 +1100

    Completed the Service Layer.

--- src/main/java/com/vega/springit/controller/AuthController.java
+++ src/main/java/com/vega/springit/controller/AuthController.java
@@ -1,11 +1,17 @@
 package com.vega.springit.controller;
 
-import org.springframework.security.core.parameters.P;
+import com.vega.springit.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
+
+    private UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/login")
     public String login() {
--- src/main/java/com/vega/springit/controller/LinkController.java
+++ src/main/java/com/vega/springit/controller/LinkController.java
@@ -4,6 +4,8 @@ import com.vega.springit.domain.Comment;
 import com.vega.springit.domain.Link;
 import com.vega.springit.repository.CommentRepository;
 import com.vega.springit.repository.LinkRepository;
+import com.vega.springit.service.CommentService;
+import com.vega.springit.service.LinkService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.annotation.Secured;
@@ -18,24 +20,24 @@ import java.util.Optional;
 
 @Controller
 public class LinkController {
-    private LinkRepository linkRepository;
-    private CommentRepository commentRepository;
+    private LinkService linkService;
+    private CommentService commentService;
     private static final Logger logger = LoggerFactory.getLogger(LinkController.class);
 
-    public LinkController(LinkRepository linkRepository, CommentRepository commentRepository) {
-        this.linkRepository = linkRepository;
-        this.commentRepository = commentRepository;
+    public LinkController(LinkService linkService, CommentService commentService) {
+        this.linkService = linkService;
+        this.commentService = commentService;
     }
 
     @GetMapping("/")
     public String list(Model model) {
-        model.addAttribute("links", linkRepository.findAll());
+        model.addAttribute("links", linkService.findAll());
         return "link/list";
     }
 
     @GetMapping("/link/{id}")
     public String read(@PathVariable Long id, Model model) {
-        Optional<Link> link = linkRepository.findById(id);
+        Optional<Link> link = linkService.findById(id);
         if( link.isPresent() ) {
             Link currentLink = link.get();
             Comment comment = new Comment();
@@ -62,7 +64,7 @@ public class LinkController {
             model.addAttribute("link", link);
             return "link/submit";
         } else {
-            linkRepository.save(link);
+            linkService.save(link);
             logger.info("New link was saved successfully");
             redirectAttributes.addAttribute("id", link.getId())
                     .addFlashAttribute("success", true);
@@ -76,7 +78,7 @@ public class LinkController {
         if (bindingResult.hasErrors()) {
             logger.info("There was a problem adding a new comment.");
         } else {
-            commentRepository.save(comment);
+            commentService.save(comment);
             logger.info("New comment was saved successfully.");
         }
         return "redirect:/link/" + comment.getLink().getId();
